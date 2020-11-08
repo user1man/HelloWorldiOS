c42b13b1eba35ab83d8bff78d5cf24ce5a1ce132
Ivan Lobanov <ia-lobanov@ya.ru>
2020-11-08 11:37:25 +0300

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Иван Лобанов on 08.11.2020.
+//  Copyright © 2020 itsolschool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
